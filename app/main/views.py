c1723953aa63a4fbd74d1f94c74040508c51b6ba
Hero <hero4barca@gmail.com>
2023-04-11 15:25:08 +0100

    login added

--- app/main/views.py
+++ app/main/views.py
@@ -2,14 +2,21 @@ from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
 from .. import db
+from flask_login import login_required
 # from .forms import NameForm
 # from ..models import User
 
 
 @main.route('/')
 def index():
-    return '<h1>Hello World!<h1/>'
+    return render_template('index.html')
+
 
 @main.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+@main.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
--- flasky.py
+++ flasky.py
@@ -16,3 +16,4 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
