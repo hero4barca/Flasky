2ebdec8b33afec9785895d79563f7a7f617dcdd5
Hero <hero4barca@gmail.com>
2023-04-12 13:24:22 +0100

    added unconfirmed account view template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for ,flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
@@ -46,3 +46,41 @@ def register():
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
 
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+        return redirect(url_for('main.index'))
+
+# redirects to 'unconfirmed view' if authenticated user is not confirmed    
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':    
+        return redirect(url_for('auth.unconfirmed'))
+    
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+                'auth/email/confirm', user=current_user,
+                token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
--- app/email.py
+++ app/email.py
@@ -9,11 +9,12 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
-    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
-                  sender=current_app.config['FLASKY_MAIL_SENDER'],
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],
                   recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[current_app, msg])
+    thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
