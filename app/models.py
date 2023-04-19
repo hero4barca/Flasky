a7cfaa07259fadb0ac406a20ded42fd784a87c0b
Hero <hero4barca@gmail.com>
2023-04-19 16:27:53 +0100

    added profile info fields and 'ping' method to User Model

--- app/models.py
+++ app/models.py
@@ -74,6 +74,12 @@ class User(UserMixin ,db.Model):
     password_hash = db.Column(db.String(128) )
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),
+    default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -82,7 +88,7 @@ class User(UserMixin ,db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             else:
                 self.role = Role.query.filter_by(default=True).first()
-        
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -131,6 +137,11 @@ class User(UserMixin ,db.Model):
     
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
     
     
 class AnonymousUser(AnonymousUserMixin):
