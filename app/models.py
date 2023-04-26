14e98207170ec8bbab4dec6fdae26690196206ff
Hero <hero4barca@gmail.com>
2023-04-26 12:50:40 +0100

    added follow model

--- app/models.py
+++ app/models.py
@@ -202,4 +202,13 @@ def load_user(user_id):
 
 login_manager.anonymous_user = AnonymousUser  
 
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+    primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+    primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
     
