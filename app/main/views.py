2f1e0579c3edc64e462bb498242a122b53265f35
Hero <hero4barca@gmail.com>
2023-04-26 15:37:02 +0100

    added 'show_all/show_followed'view functions  for posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, flash, abort, request, current_app
+from flask import render_template, redirect, url_for, flash, abort, request, make_response
 from . import main
 from .. import db
 from flask_login import login_required, current_user
@@ -17,13 +17,36 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
+    
+    # retrieve show followed from cookies
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(
+    pagination = query.order_by(
                         Post.timestamp.desc()).paginate(page=page,
                                                         per_page=10,
                                                         error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts, show_followed=show_followed, pagination=pagination)
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60) # 30 days
+    return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60) # 30 days
+    return resp
 
 
 @main.route('/user/<username>')
