bf14e6233b66f29ccb2b6635a1c58da49ab40bef
Hero <hero4barca@gmail.com>
2023-05-02 15:35:00 +0100

    updates to post permalink page to display comments

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,8 @@ from . import main
 from .. import db
 from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
-from .forms import EditProfileForm, EditProfileAdminForm, PostForm
-from app.models import Permission, User, Role, Post
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
+from app.models import Permission, User, Role, Post, Comment
 
 
 
@@ -121,10 +121,26 @@ def edit_profile_admin(id):
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
 
-@main.route('/post/<int:id>')
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('post.html', posts=[post])
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data, post=post,
+                            author=current_user._get_current_object())
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) // 10 + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(page= page,
+                                                                           per_page=10,
+                                                                            error_out=False)
+    comments = pagination.items
+    return render_template('post.html', posts=[post], form=form,
+                                            comments=comments, pagination=pagination)
+                                        
 
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
