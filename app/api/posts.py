e43505a6f1f1c52bda019ee4fda8805e587fe51c
Hero <hero4barca@gmail.com>
2023-05-05 16:44:27 +0100

    udated api.post endpoints, added permission_required to decorator

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,12 +1,26 @@
 from . import api
 from app import db
-from app.models import Post
-from flask import request, jsonify, g
+from app.models import Post, Permission
+from app.decorators import permission_required
+from flask import request, jsonify, g, url_for
 
 @api.route('/posts/', methods=['POST'])
+@permission_required(Permission.WRITE)
 def new_post():
     post = Post.from_json(request.json)
     post.author = g.current_user
     db.session.add(post)
     db.session.commit()
+    return jsonify(post.to_json()), 201, \
+                        {'Location': url_for('api.get_post', id=post.id)}
+
+
+@api.route('/posts/')
+def get_posts():
+    posts = Post.query.all()
+    return jsonify({ 'posts': [post.to_json() for post in posts] })
+
+@api.route('/posts/<int:id>')
+def get_post(id):
+    post = Post.query.get_or_404(id)
     return jsonify(post.to_json())
