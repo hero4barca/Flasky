8900f6fc36f57a9ed0fbab2cbcc5043ecb1df0ac
Hero <hero4barca@gmail.com>
2023-05-05 17:16:28 +0100

    api.get_posts updateds with pagination

--- app/api/posts.py
+++ app/api/posts.py
@@ -18,8 +18,22 @@ def new_post():
 
 @api.route('/posts/')
 def get_posts():
-    posts = Post.query.all()
-    return jsonify({ 'posts': [post.to_json() for post in posts] })
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate( page=page, per_page=10, error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1)
+        next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev_url': prev,
+        'next_url': next,
+        'count': pagination.total
+        })
+
 
 @api.route('/posts/<int:id>')
 def get_post(id):
